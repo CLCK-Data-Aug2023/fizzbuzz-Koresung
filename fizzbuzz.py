2d35949d3e490b55ed8b63437e8fb3181daf7651
Moses <koresung@gmail.com>
2023-11-02 18:47:58 -0400

    The b in FizzBuzz was lowercase before

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 # add your code here
 for num in range(1, 101):
     if num % 3 == 0 and num % 5 == 0:
-        print("Fizzbuzz")
+        print("FizzBuzz")
     elif num % 3 == 0:
         print("Fizz")
     elif num % 5 == 0:
